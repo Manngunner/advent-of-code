fae5e4537ca3d16be5b9caba9344d3056c0e6012
Manngunner <46092339+Manngunner@users.noreply.github.com>
2022-12-04 13:46:15 +0000

    If statement weightloss

--- Day 4/day4-task2.py
+++ Day 4/day4-task2.py
@@ -11,19 +11,10 @@ with open ("Day 4/input.txt", "r", encoding="utf8") as inputfile:
         # Elf 2 range
         elf2low = int(cleaning.group(3))
         elf2high = int(cleaning.group(4))
-        # Apparently if both sets contain the other this still counts :(
-        #if elf1low == elf2low and elf1high == elf2high:
-        #    continue
-        if elf1low <= elf2low <= elf1high:
+        if (elf1low <= elf2low <= elf1high) or (elf1low <= elf2high <= elf1high):
             overlapcount += 1
             continue
-        if elf1low <= elf2high <= elf1high:
-            overlapcount += 1
-            continue
-        if elf2low <= elf1low <= elf2high:
-            overlapcount += 1
-            continue
-        if elf2low <= elf1high <= elf2high:
+        if (elf2low <= elf1low <= elf2high) or (elf2low <= elf1high <= elf2high):
             overlapcount += 1
             continue
     print(overlapcount)
