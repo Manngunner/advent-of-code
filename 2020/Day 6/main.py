696a7740baa5a21da907470f3417a847319bae67
Manngunner <46092339+Manngunner@users.noreply.github.com>
2025-12-01 23:10:04 +0000

    Complete

--- 2020/Day 6/main.py
+++ 2020/Day 6/main.py
@@ -8,13 +8,13 @@ with Path("2020/Day 6/input").open("r") as file:
     standard_input: list[list[str]] = [group.split("\n") for group in file_parse]
 
 
-def main(groups: list[list[str]]) -> tuple[int]:
+def main(groups: list[list[str]]) -> tuple[int, int]:
     total_yes = 0
     total_all = 0
     for group in groups:
         forms = [set(x) for x in group]
-        yeses = set.union(*forms)
-        unanimous = set.intersection(*forms)
+        yeses: set[str] = set.union(*forms)
+        unanimous: set[str] = set.intersection(*forms)
         total_yes += len(yeses)
         total_all += len(unanimous)
 
@@ -24,5 +24,5 @@ def main(groups: list[list[str]]) -> tuple[int]:
 if __name__ == "__main__":
     sample = main(sample_input)
     standard = main(standard_input)
-print(f"Sample Task 1: {sample[0]}, Sample Task 2: {sample[1]}")
-print(f"Actual Task 1: {standard[0]}, Actual Task 2: {standard[1]}")
+    print(f"Sample Task 1: {sample[0]}, Sample Task 2: {sample[1]}")
+    print(f"Actual Task 1: {standard[0]}, Actual Task 2: {standard[1]}")
